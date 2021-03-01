0cb6e9e4f1b22cf55ea48b187959b938cd96e264
Irfan Varren <irfanvarren7@gmail.com>
2021-03-01 18:37:31 +0700

    first commit

---
+++ app/src/main/java/com/irfanvarren/uas182101955/MahasiswaModel.java
@@ -0,0 +1,34 @@
+package com.irfanvarren.uas182101955;
+
+public class MahasiswaModel {
+    private String NIM, Nama, JenisKelamin, JP;
+
+    public MahasiswaModel() {
+
+    }
+
+    public String getNIM() {
+        return NIM;
+    }
+
+    public void setNIM(String NIM) {
+        this.NIM = NIM;
+    }
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        Nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return JenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        JenisKelamin = jenisKelamin;
+    }
+
+}
